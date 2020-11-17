7dc0626c42043e796b3374f4b703c6e96c4a5311
Vinay <pvinayvinu20@gmail.com>
2020-11-17 10:48:09 +0530

    Update Profile.tsx

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Col, Row, Container } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import { ProfileSetup } from "../../core/types";
@@ -12,7 +12,7 @@ import classNames from "classnames";
 
 export const Profile = (props: any) => {
   let profileConst: ProfileSetup = props.appConst.profile;
-  const { width, height } = useResponsive();
+  const { width } = useResponsive();
   let profileClass = classNames({
     "is-mobile": width < 1200,
     "profile-container": true,
